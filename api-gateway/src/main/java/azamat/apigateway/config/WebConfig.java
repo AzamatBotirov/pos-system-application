f05357c1b911bcd102dda584c82efec9f32f9834
AzamatBotirov <azamat.01.0112@gmail.com>
2023-10-06 16:33:29 +0500

    add console cloud

---
+++ api-gateway/src/main/java/azamat/apigateway/config/WebConfig.java
@@ -0,0 +1,6 @@
+package azamat.apigateway.config;
+
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+public class WebConfig  {
+}
---
+++ api-gateway/src/test/java/azamat/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package azamat.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
